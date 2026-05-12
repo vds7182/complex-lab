996545a4080f1bece80b5715a964b3b9ea1c3b1a
vds7182 <vds71828@gmail.com>
2026-05-12 15:32:25 +0300

    Lab3 finish

---
+++ src/main/java/com/example/backend/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.backend.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String message;
+    private String path;
+}
